5f019578dd0e12f3124d243d3081c62b31d86826
jokeway <jokeway@gmail.com>
2021-02-04 14:38:27 +0800

    [IMP] update pulsar

--- pulsar/scripts/start_mainbk.sh
+++ pulsar/scripts/start_mainbk.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-bin/pulsar initialize-cluster-metadata --cluster pulsar-cluster-1 --zookeeper zoo1:2181 --web-service-url localhost:8080 --configuration-store zoo1:2181 --broker-service-url pulsar://broker1:6650 > logs/init-metadata.log 2>&1
+bin/pulsar initialize-cluster-metadata --cluster pulsar-cluster-1 --zookeeper macula-cloud-pulsar-zookeeper1:2181 --web-service-url localhost:8080 --configuration-store macula-cloud-pulsar-zookeeper1:2181 --broker-service-url pulsar://macula-cloud-pulsar-broker1:6650 > logs/init-metadata.log 2>&1
 
 bin/bookkeeper bookie > logs/bookkeeper.log 2>&1
